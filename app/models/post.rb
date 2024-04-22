1c0eb93479c04e0b8523a128b3699a914acd2448
JaValderaContador <javier1gdvc@gmail.com>
2024-04-22 16:21:47 -0400

    References v2.0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
-    belongs_to :post
+    has_many :posts_tags
+    has_many :tags, through: :post_tags
     has_many :posts
-    #has_many :tags
+    belongs_to :post
 end
