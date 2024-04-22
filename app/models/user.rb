37f10a19f3e97aa3adba098e67bcd9e7b20d0ce5
JaValderaContador <javier1gdvc@gmail.com>
2024-04-22 16:58:08 -0400

    User mail validation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
     validates :name, presence: {message: "Please anter a name, I don't think you're nameless"}
-    validates :email, presence: {message: "You  must enter your email address"}, uniqueness: {message: "This emial already exist. (You late LMAO)"}, format: {message: "Email is not valid, please try again"}
+    validates :email, presence: {message: "You  must enter your email address"}, uniqueness: {message: "This emial already exist. (You late LMAO)"}, format: {with: EMAIL_REGEX, message: "Email is not valid, please try again"}
     validates :password, presence: {message: "YOU NEED A PASSWORD!!!"}, length: {minimum: 6, message: "Password must have at least 6 chars"}
 
     has_many :posts
